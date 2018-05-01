3e3db6366dac0d0d46e068a8db84beca60d92e3c
escamsam000 <32393078+escamsam000@users.noreply.github.com>
2018-05-01 14:58:17 -0500

    Add files via upload

---
+++ 1a.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+series_obj = Series(np.arange(8),
+                    index=['row 1', 'row 2', 'row 3',
+                           'row 4', 'row 5', 'row 6',
+                           'row 7', 'row 8'])
+np.random.seed(25)
+DF_obj = DataFrame(np.random.rand(36).reshape((6, 6)),
+                   index=['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6'],
+                   columns=['column 1', 'column 2', 'column 3',
+                            'column 4', 'column 5', 'column 6'])
+print(DF_obj)
+print(DF_obj.ix[['row 2', 'row 5'], ['column 2', 'column 5']])
+print(series_obj['row 3': 'row 7'])
+print(DF_obj < .2)
+print(series_obj[series_obj > 6])
+series_obj['row 2', 'row 5', 'row 8'] = 8
+print(series_obj)
